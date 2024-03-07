9efdd24f7eb0c8ca8d9088a79629ac7d0778d854
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-07 22:54:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './todoReducer';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+export type RootState = ReturnType<typeof store.getState>;
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
